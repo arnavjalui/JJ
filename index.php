8ab5c3cb2fd5dd0077dcd783d518372a3f736198
arnavjalui <arnavjalui@gmail.com>
2017-06-21 13:34:55 +0530

    Navbar layout changed: Contact line height reduced

---
+++ index.php
@@ -0,0 +1,145 @@
+<!DOCTYPE html>
+<html lang="en-US">
+   <head>
+      <meta charset="UTF-8">
+      <meta name="viewport" content="width=device-width" />
+      <title>JJ Business Website</title>           
+      <!-- CUSTOM STYLE -->
+      <link href='http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700,800&subset=latin,latin-ext' rel='stylesheet' type='text/css'>
+      
+       <style>
+
+           body {
+               background:none repeat scroll 0 0 #d3d3d3;
+               font-size:100%;
+               font-family:"Open Sans",Arial,sans-serif;
+               color:#444;
+               -webkit-box-sizing:border-box;
+               -moz-box-sizing:border-box;
+               box-sizing:border-box;
+               margin:0;
+               overflow: auto;
+            }
+           .line {
+               margin:0 auto;
+               max-width:71.25em;
+               padding:0 0.625em;
+           }
+
+           header {
+	           border-bottom: 4px solid rgba(0, 40, 58, 0.53);
+               position: fixed;
+               width: 100%;
+               z-index: 10;
+           }
+           #topbar {
+               background: none repeat scroll 0 0 #012232;
+               padding: 0.3125em 0;
+               border-bottom: 1px solid #001925;
+           }
+           #topbar p {
+               color: #fff;
+               font-size: 0.75em;
+               opacity: 0.5;
+               margin: 0;
+               position: relative;
+               top: 2px;
+           }
+           #topbar p strong {
+               font-weight: 600;
+           }
+           nav {
+	           background: none repeat scroll 0 0 #00283a;
+           }
+           
+           .top-nav li a {
+	           border-top: 2px solid #00283a;
+               color: #fff;
+               font-size: 0.75em;
+	           font-weight: 600;
+               padding: 2.0em 1.5em;
+               text-transform: uppercase;
+               display:block;
+               color:#fff; 
+               transition: all 0.20s linear 0s;
+               -o-transition: all 0.20s linear 0s;
+               -ms-transition: all 0.20s linear 0s;
+               -moz-transition: all 0.20s linear 0s;
+               -webkit-transition: all 0.20s linear 0s;
+           }
+           .top-nav li a:hover, .top-nav li.active-item a {
+               background: none repeat scroll 0 0 #00283a;
+               border-top: 2px solid #92c500;
+               color: #92c500;
+               position: relative;
+           }
+           .line:after,nav:after,.center:after,.box:after {
+               clear:both;
+               content:".";
+               display:block;
+               height:0;
+               line-height:0;
+               visibility:hidden;
+           }
+           .top-nav ul {
+               padding:0;
+               margin-top: 0;
+               margin-bottom: 0;
+           }
+           .top-nav li {
+               float:left;
+               list-style:none outside none;
+               cursor:pointer;
+           }
+           p.logo {
+	           color: #fff;
+               font-size: 2.5em;
+               font-weight: 300;
+               position: relative;
+               text-transform: uppercase;
+               margin: 0.2em 0 0.4em;
+           }
+           p.logo strong {
+               font-weight: 700;
+           }
+
+           .top-nav .active-item a {background:none repeat scroll 0 0 #999;}
+           .s-12{
+               float:left;
+               position:relative;
+           }
+           .right {float:right;}
+      </style>
+   </head>
+<body class="size-1140">
+      <!-- TOP NAV-->
+      <header>
+        <div id="topbar">
+            <div class="line">
+                  <p>CONTACT US: <strong>0800 200 200</strong> | <strong>jahanvijasani.46@gmail.com</strong></p>
+            </div>  
+         </div> 
+         <nav>
+             <div class="line">
+                <div class="s-12">
+                  <p class="logo"><strong>One</strong>page</p>
+               </div>
+                
+                <div class="top-nav">
+                    <ul class="right">
+                        <li class="active-item"><a href="#carousel">Home</a></li>
+                        <li><a href="#features">Features</a></li>
+                        <li><a href="#about-us">About Us</a></li>
+                        <li><a href="#our-work">Our Work</a></li>
+                        <li><a href="#services">Services</a></li>
+                        <li><a href="#contact">Contact</a></li>
+                    </ul>
+                </div>
+             </div>
+        </nav>
+     </header>
+    <section>
+        
+    </section>
+    
+        
